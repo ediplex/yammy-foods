dcad087cd157070d0270b9566a77b16f7d0994df
tkhs-c <takahashi@ediplex.com>
2016-07-14 14:39:26 +0900

    class名変更・調整

--- js/tabs.js
+++ js/tabs.js
@@ -1,6 +1,6 @@
   $(function(){
       $("#event-tabs-menu li a").on("click", function() {
-          $("#event-list__tab-boxes div").hide();
+          $("#event-list__tab-boxes　div").hide();
           $($(this).attr("href")).fadeToggle();
       });
       return false;
