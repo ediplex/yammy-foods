8d2b7e84f716d402d58fcd84c244235382c7eaec
tkhs-c <takahashi@ediplex.com>
2016-08-25 18:36:56 +0900

    高岡さん

--- js/tabs.js
+++ js/tabs.js
@@ -1,11 +1,16 @@
   $(function(){
-      $(".event-tabs-menu-item__link").on("click", function(ev) {
-          ev.preventDefault();
-          var tab = $($(this).attr("href"));
-          if (!tab.is(':visible')) {
-            $(".event-tab").hide();
-            tab.fadeToggle();
-          }
-      });
-      return false;
-  });
+    var activeCls = "event-tabs-menu-item__active";
+    var tabMenuItems = $(".event-tabs-menu-item");
+    $(".event-tabs-menu-item__link").on("click", function(ev) {
+        ev.preventDefault();
+        tabMenuItems.removeClass(activeCls);
+        var $this = $(this);
+        $this.parent('.event-tabs-menu-item').addClass(activeCls);
+        var tab = $($this.attr("href"));
+        if (!tab.is(':visible')) {
+          $(".event-tab").hide();
+          tab.fadeToggle();
+        }
+    });
+    return false;
+});
